bebdf5132720c5402e8e3452cea8bdc9ae79f62b
Prashant Jyoti <jtprashant@gmail.com>
2015-10-24 10:22:46 +0530

    changes

--- README.md
+++
@@ -1,4 +0,0 @@
-# Practice
-My local repo to practice things
-
-21/10/2015 : Beginnings :)
--- prac2.py
+++
@@ -1,9 +0,0 @@
-def take_out_digits(number):
-    list = []
-    for i in range(0, len(number)):
-        list.append(number[i])
-    return list
-
-number = input("Enter a number: ")
-result = take_out_digits(number)
-print (result)
