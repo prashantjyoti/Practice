7b6fd33dbb801e0538e251079b0912080def1ca5
Prashant Jyoti <jtprashant@gmail.com>
2015-10-24 09:42:39 +0530

    program almost complete

--- thirteen_adjacent_digits_8.py
+++ thirteen_adjacent_digits_8.py
@@ -20,44 +20,42 @@ thousandDigits = ("73167176531330624919225119674426574742355349194934"+
                   "05886116467109405077541002256983155200055935729725"+
                   "71636269561882670428252483600823257530420752963450")
 
-maximum = 0
-
-def longestString(substring):
-    return substring
 
 def deriveSubstring(string):
     substring, currentMaximum, resultList = [], 0, []
-    for i in range(0,988):
-        for j in range(i, i+13):
-            substring = string[i:i+13]
+    longestString = ""
+    for i in range(0,938):
+        product = 1
+        #print ()
+        #print ("current iteration: ")
+        #print (i)
+        substring = string[i:i+13]
+        #print ("substring: ")
+        #print (substring)
+        if substring.find('0') == -1:
+            for i in substring:
+                product *= int(i)
+        #print ("product: ")
+        #print (product)
+        #print ("current max: ")
+        #print (currentMaximum)
+        if product > currentMaximum:
+            longestString = substring
+            currentMaximum = product
 
-            # print("length of substring: ")
-            # print(len(substring))
-            # print("substring: ")
-            # print(substring)
-            resultList = multiplyAndCompare(substring, currentMaximum)
-
+    resultList.append(currentMaximum)
+    resultList.append(longestString)
     return resultList
 
-def multiplyAndCompare(string, number):
-    product, maxi, resultList = 1, number, [0,0]
-    for i in string:
-        if(int(i)) == 0 :
-            break
-        else:
-            #for i in range(0,14):
-            product *= int(i)
 
-    if product > maxi:
-        resultList[0] = product
-        resultList[1] = string
-
-    return resultList
 
 results = (deriveSubstring(thousandDigits))
+print ()
 print("maximum product is: ")
 print(results[0])
 print("longest string is: ")
 print(results[1])
+
+
 
 
