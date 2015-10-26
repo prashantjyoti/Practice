21e5115a073263de27e194a13b251e18191b8cfc
Prashant Jyoti <jtprashant@gmail.com>
2015-10-26 18:22:24 +0530

    create

---
+++ DemoProj/adjacent_numbers_product_11.py
@@ -0,0 +1,23 @@
+def right_lower_diagonal_product(index1, index2):
+  return product
+  
+def right_upper_diagonal_product(index1, index2):
+  return product
+  
+def left_lower_diagonal_product(index1, index2):
+  return product
+  
+def left_lower_diagonal_product(index1, index2):
+  return product
+  
+def acrossLtoR_product(index1, index2):
+  return product
+  
+def acrossRtoL_product(index1, index2):
+  return product
+  
+def horizontalBtoU_product(index1, index2):
+  return product
+  
+def horizontalUtoB_product(index1, index2):
+  return product
