150e57ad12eea3aebba78d7d6777118666a3e3cc
Prashant Jyoti <jtprashant@gmail.com>
2015-10-26 18:33:38 +0530

    update

--- DemoProj/adjacent_numbers_product_11.py
+++ DemoProj/adjacent_numbers_product_11.py
@@ -1,3 +1,5 @@
+if 00 comes in any direction then dont multiiply in that direction
+
 def right_lower_diagonal_product(index1, index2):
   return product
   
@@ -21,3 +23,7 @@ def horizontalBtoU_product(index1, index2):
   
 def horizontalUtoB_product(index1, index2):
   return product
+
+make a list
+for any given number, find all the products, append them in a list
+find max of list, store index of number and its max product(if greater than prev max) in any direction in global variables
