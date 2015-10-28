0c219165d1939eef617dab94730186c07bb3c1d6
Prashant Jyoti <jtprashant@gmail.com>
2015-10-28 15:52:13 +0530

    update

--- DemoProj/adjacent_numbers_product_11.py
+++ DemoProj/adjacent_numbers_product_11.py
@@ -1,31 +1,33 @@
-make a list
+"""make a list
 for any given number, find all the products, append them in a list
-find max of list, store index of number and its max product(if greater than prev max) in any direction in global variables
+find max of list, store index of number and its max product(if greater than prev max)
+in any direction in global variables """
 
+#def initializeMatrix():
 f = open('data_11.txt', 'r')
-#print (f)
-#for line in f:
+    #print (f)
+    #for line in f:
     #print (line)
 list_file_data = list(f)
-#print(list_file_data[0])
-#print(list_file_data)
-#str = list_file_data[0]
-# for i in range(len(list_file_data)):
-#     listA = list_file_data[i].split()
-#     print(listA)
-#     print(listA[0])
-#print (list_file_data[0].split())
-#print(len(abc))
+    #print(list_file_data[0])
+    #print(list_file_data)
+    #str = list_file_data[0]
+    # for i in range(len(list_file_data)):
+    #     listA = list_file_data[i].split()
+    #     print(listA)
+    #     print(listA[0])
+    #print (list_file_data[0].split())
+    #print(len(abc))
 
-#[[0 for x in range(cols_count)] for x in range(rows_count)]
+    #[[0 for x in range(cols_count)] for x in range(rows_count)]
 Matrix = [[0 for x in range(20)] for x in range(20)]
-#
+
 for i in range(20):
     list_numbers_in_a_line = list_file_data[i].split()
     for j in range(20):
-         Matrix[i][j] = list_numbers_in_a_line[j]
+        Matrix[i][j] = list_numbers_in_a_line[j]
 
-#print (Matrix)
+    #print (Matrix)
 for i in range(20):
     print(Matrix[i])
 
@@ -82,7 +84,7 @@ def right_lower_diagonal_product(Matrix, index1, index2):
         product = 1
         j = index2
         for i in range(index1,index1+4):
-            product *= Matrix[i][j]
+            product *= int(Matrix[i][j])
             j += 1
         return product
     else:
@@ -93,7 +95,7 @@ def right_upper_diagonal_product(Matrix, index1, index2):
         product =1
         j = index2
         for i in range(index1,index1-4,-1):
-            product *= Matrix[i-1][j+1]
+            product *= int(Matrix[i-1][j+1])
             j += 1
         return product
     else:
@@ -104,7 +106,7 @@ def left_lower_diagonal_product(Matrix, index1, index2):
         product = 1
         j = index2
         for i in range(index1,index1+4):
-            product *= Matrix[i+1][j-1]
+            product *= int(Matrix[i+1][j-1])
             j += 1
         return product
     else:
@@ -115,7 +117,7 @@ def left_upper_diagonal_product(Matrix, index1, index2):
         product = 1
         j = index2
         for i in range(index1,index1-4,-1):
-            product *= Matrix[i-1][j-1]
+            product *= int(Matrix[i-1][j-1])
             j += 1
         return product
     else:
@@ -125,7 +127,7 @@ def LtoR_product(Matrix, index1, index2):
     if(hasLtoR(index1,index2)):
         product = 1
         for i in range(index2,index2+4):
-            product *= Matrix[index1][i]
+            product *= int(Matrix[index1][i])
         return product
     else:
         return 0
@@ -134,7 +136,7 @@ def RtoL_product(Matrix, index1, index2):
     if(hasRtoL(index1,index2)):
         product = 1
         for i in range(index2,index2-4,-1):
-            product *= Matrix[index1][i]
+            product *= int(Matrix[index1][i])
         return product
     else:
         return 0
@@ -143,7 +145,7 @@ def BtoU_product(Matrix, index1, index2):
     if(hasBtoU(index1,index2)):
         product = 1
         for i in range(index1,index1-4,-1):
-            product *= Matrix[i][index2]
+            product *= int(Matrix[i][index2])
         return product
     else:
         return 0
@@ -152,7 +154,37 @@ def UtoB_product(Matrix, index1, index2):
     if(hasUtoB(index1,index2)):
         product = 1
         for i in range(index1,index1+4):
-            product *= Matrix[i][index2]
+            product *= int(Matrix[i][index2])
         return product
     else:
         return 0
+
+productList = []
+for i in range(8):
+    productList.append(0)
+
+ansList = []
+for i in range(4):
+    ansList.append(0)
+
+globalMax = 0
+
+for i in range(len(Matrix)):
+    for j in range(len(Matrix)):
+        productList[0] = LtoR_product(Matrix,i,j)
+        productList[1] = RtoL_product(Matrix,i,j)
+        productList[2] = BtoU_product(Matrix,i,j)
+        productList[3] = UtoB_product(Matrix,i,j)
+        productList[4] = right_lower_diagonal_product(Matrix,i,j)
+        productList[5] = right_upper_diagonal_product(Matrix,i,j)
+        productList[6] = left_lower_diagonal_product(Matrix,i,j)
+        productList[7] = left_upper_diagonal_product(Matrix,i,j)
+
+        if(max(productList) > globalMax):
+            globalMax = max(productList)
+            ansList[0] = max(productList)
+            ansList[1] = i
+            ansList[2] = j
+            ansList[3] = Matrix[i][j]
+
+print (ansList)
